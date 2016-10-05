5a0c6234d7dc04864aff2517793516a6195c24cf
Patrick Lee <patrick.lee@u-psud.fr>
2016-10-05 15:26:23 +0200

    Displaced the empty list

--- angular_js_mod3_solution/app.js
+++ angular_js_mod3_solution/app.js
@@ -47,7 +47,6 @@ MenuSearchService.$inject = ['$http', '$filter']
 function MenuSearchService($http, $filter){
 	var service = this;
 	service.getMatchedMenuItems = function (searchTerm){
-		
 		return $http({
 			method: "GET",
 			url:("https://davids-restaurant.herokuapp.com/menu_items.json"),
