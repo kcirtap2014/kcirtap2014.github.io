2de1912b76e94b4370819f7687fe44e3049342bf
Patrick Lee <patrick.lee@u-psud.fr>
2016-10-09 03:35:26 +0200

    Deleted log

--- angular_js_mod4_solution/src/item-detail.controller.js
+++ angular_js_mod4_solution/src/item-detail.controller.js
@@ -10,7 +10,6 @@ ItemDetailController.$inject = ['MenuDataService','items'];
 function ItemDetailController(MenuDataService, items) {
   var iD = this;
   iD.items = items;
-  console.log(iD.items)
 }
 
 })();
